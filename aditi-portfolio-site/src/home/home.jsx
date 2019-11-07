a726f38f3b98b37ad76996858d6c8bcffd6d3e02
Aditi <asaki444@gmail.com>
2019-11-06 22:29:12 -0500

    added some styling to the navigation bar for mobile

--- aditi-portfolio-site/src/home/home.jsx
+++ aditi-portfolio-site/src/home/home.jsx
@@ -11,7 +11,7 @@ export default function Home (){
         <div className="home-page">
             <div className="welcome-container">
                 <div className="welcome-text">
-                Happy to e-meet you, my name is Aditi, a community focused software engineer in the city!
+                Happy to e-meet you, my name is Aditi, a community focused software engineer in the New York City!
                 </div>
            
             </div>
--- aditi-portfolio-site/src/navigationBar/navigationBar.jsx
+++ aditi-portfolio-site/src/navigationBar/navigationBar.jsx
@@ -2,49 +2,73 @@ import React from 'react';
 import './navigation.scss';
 import Home from '../home/home';
 import AditiLogo from '../aditi-logo/aditi-logo.png';
+import {FaBars} from 'react-icons/fa';
 
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+
+export default class NavigationBar extends React.Component {
+  constructor(props) {
+    super(props);
+  this.state = {
+     toggleMobileNav: false
+  }
+}
+
+
+
+render (){
+
+   let showMobileNav = () => {
+     this.setState({
+        toggleMobileNav: !this.state.toggleMobileNav
+    }) 
+    }
   
+  return (
+    <Router>
+      <div>
 
-export default function NavigationBar(){
-    return(
-     <Router>
-         <div>
-           
-             <div className="image-container">
-            <img src={AditiLogo}/>
+        <div className="image-container">
+          <img src={AditiLogo}/>
+        </div>
+
+        <div className="mobile-navigation-bars"  onClick={showMobileNav}>
+          <FaBars />
+        </div>
+        <div className={this.state.toggleMobileNav ? "home-navigation-bar show-nav-bar" : "home-navigation-bar" }>
+
+          <div className="nav-item">
+            <Link to="/" className="nav-text">
+              Home
+            </Link>
           </div>
-         <div className="home-navigation-bar">
+          <div className="nav-item">
+            <Link to="/about-me" className="nav-text">
+              About Me
+            </Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/portfolio" className="nav-text">Portfolio</Link>
+          </div>
+          <div className="nav-item">
+            <Link to="/community-involvement" className="nav-text">Community</Link>
+          </div>
+          <div className="nav-item ">
+            <Link to="/contact" className="nav-text">
+              Contact Me
+            </Link>
+          </div>
+        </div>
        
-          <div className="nav-item">
-          <Link to="/" className="nav-text"> Home </Link> 
-          </div>
-          <div className="nav-item">
-            <Link to="/about-me" className="nav-text"> About Me </Link> 
-          </div>
-      <div className="nav-item">
-        <Link to="/portfolio" className="nav-text">Portfolio</Link>
       </div>
-      <div className="nav-item">
-         <Link to="/community-involvement" className="nav-text">Community</Link> 
-      </div>
-    <div className="nav-item ">
-     <Link to="/contact" className="nav-text" > Contact Me </Link>
-    </div>
-         </div>
-         </div>
-<Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-     
-        </Switch>
-     </Router>
-    )
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+      </Switch>
+    </Router>
+  )
+  }
 
 }
