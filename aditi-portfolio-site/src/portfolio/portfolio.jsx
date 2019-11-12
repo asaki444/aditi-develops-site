a62f13e16606f5ae328eb6cd599d471e1b4bf738
Aditi <asaki444@gmail.com>
2019-11-11 23:20:43 -0500

    added screenshots of portfolio pics to site

--- aditi-portfolio-site/src/portfolio/portfolio.jsx
+++ aditi-portfolio-site/src/portfolio/portfolio.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './portfolio.scss';
-
+import PortfolioWebsite from '../images/website.png';  
+import ProfessionalWebsite from '../images/professional_work.png';
 
 
 export default function Portfolio (){
@@ -11,7 +12,28 @@ export default function Portfolio (){
              <div className="projects-title">
                 My Projects
              </div>
-             
+             <div className="projects-container">
+                 <div className="project">
+                   <img src={PortfolioWebsite}/>
+                   <p>
+                      Frontend: React/Sass 
+                   </p>
+                   <p>
+                      <a href="https://github.com/asaki444/aditi-develops-site">
+                      Source Code
+                          </a>
+                   </p>
+                 </div>
+                 <div className="project">
+                 <img src={ProfessionalWebsite}/>
+                   <p>FrontEnd: React/Sass</p>
+                   <p> Backend: Java</p>
+                   <p>Tools: Google Analytics for A/B test</p>
+                 </div>
+                 <div className="project">
+
+                 </div>
+            </div>
         </div>
      )
 }
