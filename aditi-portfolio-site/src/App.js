b0291d6b57b222211f774824aa8acadf786d3649
Aditi <asaki444@gmail.com>
2019-12-16 15:19:06 -0500

    added favicon

--- aditi-portfolio-site/src/App.js
+++ aditi-portfolio-site/src/App.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import NavigationBar from './navigationBar/navigationBar';
-import {
-  BrowserRouter as Router,
-} from 'react-router-dom';
 import AboutMe from './aboutMe/aboutMe';
 import Home from './home/home';
 import Portfolio from './portfolio/portfolio';
